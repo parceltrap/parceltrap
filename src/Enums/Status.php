c96b62c90590322e90e8a8f438afd484ec20b2e0
Owen Voke <development@voke.dev>
2022-02-14 11:04:13 +0000

    feat: add description method to `Status` enum

--- src/Enums/Status.php
+++ src/Enums/Status.php
@@ -11,4 +11,15 @@ enum Status: string
     case Delivered = 'delivered';
     case Failure = 'failure';
     case Unknown = 'unknown';
+
+    public function description(): string
+    {
+        return match($this) {
+            self::Pre_Transit => 'Pre-Transit',
+            self::In_Transit => 'In Transit',
+            self::Delivered => 'Delivered',
+            self::Failure => 'Failure',
+            self::Unknown => 'Unknown',
+        };
+    }
 }
