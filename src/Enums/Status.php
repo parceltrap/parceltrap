6827a5959eab63a999e7390818699ef708c051ff
Owen Voke <development@voke.dev>
2022-02-14 11:34:07 +0000

    feat: add extra options to `Status` enum

--- src/Enums/Status.php
+++ src/Enums/Status.php
@@ -6,19 +6,23 @@ namespace ParcelTrap\Enums;
 
 enum Status: string
 {
+    case Pending = 'pending';
     case Pre_Transit = 'pre_transit';
     case In_Transit = 'in_transit';
     case Delivered = 'delivered';
     case Failure = 'failure';
+    case Not_Found = 'not_found';
     case Unknown = 'unknown';
 
     public function description(): string
     {
         return match($this) {
+            self::Pending => 'Pending',
             self::Pre_Transit => 'Pre-Transit',
             self::In_Transit => 'In Transit',
             self::Delivered => 'Delivered',
             self::Failure => 'Failure',
+            self::Not_Found => 'Not Found',
             self::Unknown => 'Unknown',
         };
     }
