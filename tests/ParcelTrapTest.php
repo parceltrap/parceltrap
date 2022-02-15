0978663b80744a5e1afa5ebd6950cb340798b67c
Owen Voke <development@voke.dev>
2022-02-15 10:35:01 +0000

    tests: cover all lines with more tests

--- tests/ParcelTrapTest.php
+++ tests/ParcelTrapTest.php
@@ -12,11 +12,22 @@ it('can instantiate ParcelTrap', function () {
         ->toBeInstanceOf(ParcelTrap::class);
 });
 
+it('can add multiple drivers to ParcelTrap', function () {
+    $client = ParcelTrap::make(['null' => NullDriver::make([])]);
+    $client->addDriver('null2', NullDriver::make([]));
+
+    expect($client)->hasDriver('null2')->toBeTrue();
+});
+
 it('can retrieve a driver from ParcelTrap', function () {
     expect(ParcelTrap::make(['null' => NullDriver::make([])]))
-        ->driver('null')
-        ->toBeInstanceOf(NullDriver::class);
+        ->hasDriver('null')->toBeTrue()
+        ->driver('null')->toBeInstanceOf(NullDriver::class);
 });
+
+it('throws an exception when a driver can\'t be found in ParcelTrap', function () {
+    ParcelTrap::make([])->driver('abc');
+})->throws(InvalidArgumentException::class);
 
 it('can set a default ParcelTrap driver', function () {
     $client = ParcelTrap::make(['null' => NullDriver::make([])]);
@@ -26,11 +37,20 @@ it('can set a default ParcelTrap driver', function () {
     expect($client->driver())->toBeInstanceOf(NullDriver::class);
 });
 
+it('throws an exception when attempting to set a non-existant default driver in ParcelTrap', function () {
+    ParcelTrap::make([])->setDefaultDriver('null');
+})->throws(InvalidArgumentException::class);
+
+it('throws an exception when a default driver hasn\'t been set in ParcelTrap', function () {
+    ParcelTrap::make([])->driver();
+})->throws(InvalidArgumentException::class);
+
 it('can call `find` on a ParcelTrap driver', function () {
     expect(ParcelTrap::make(['null' => NullDriver::make([])])->driver('null')->find('abcdefg'))
         ->toBeInstanceOf(TrackingDetails::class)
         ->identifier->toBe('abcdefg')
-        ->status->toBe(Status::Unknown);
+        ->status->toBe(Status::Unknown)
+        ->status->description()->toBe('Unknown');
 });
 
 it('can call `find` on the default ParcelTrap driver', function () {
