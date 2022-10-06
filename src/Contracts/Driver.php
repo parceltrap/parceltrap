15eba2e635b4cdfb98f3a3d39d5a9aa2e737d25d
Bradie <44430471+bradietilley@users.noreply.github.com>
2022-10-07 07:25:59 +0800

    chore: order imports to satisfy pint

--- src/Contracts/Driver.php
+++ src/Contracts/Driver.php
@@ -3,13 +3,14 @@
 namespace ParcelTrap\Contracts;
 
 use ParcelTrap\DTOs\TrackingDetails;
-use ParcelTrap\Exceptions\ApiLimitReachedException;
 use ParcelTrap\Exceptions\ApiAuthenticationFailedException;
+use ParcelTrap\Exceptions\ApiLimitReachedException;
 
 interface Driver
 {
     /**
      * @param  array<string, mixed>  $parameters
+     *
      * @throws ApiAuthenticationFailedException
      * @throws ApiLimitReachedException
      */
