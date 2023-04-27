3675e24a395fa4b8b7698d93ce06561a2af1cf7c
Owen Voke <development@voke.dev>
2023-04-27 14:07:39 +0100

    chore: fix facade auto-completion to be static

--- src/Facades/ParcelTrap.php
+++ src/Facades/ParcelTrap.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace ParcelTrap\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use ParcelTrap\Contracts\Driver;
+use ParcelTrap\DTOs\TrackingDetails;
 
-/** @mixin \ParcelTrap\ParcelTrap */
+/**
+ * @method static Driver driver(string|null $driver = null)
+ * @method static TrackingDetails find(string $identifier, array $parameters = [])
+ */
 class ParcelTrap extends Facade
 {
     protected static function getFacadeAccessor(): string
